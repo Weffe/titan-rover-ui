bd0b8c8d9a0839198caf534ff7a75c87f69f3a5f
Rogelio Negrete <rogelio_negrete@live.com>
2016-10-20 18:46:03 -0700

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import Router from 'react-router';  
+import { DefaultRoute, IndexLink, Link, Route, RouteHandler } from 'react-router';
+
+let routes = (
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Home} />
+          <Route path="module1" component={Module1} />
+          <Route path="module2" component={Module2} />
+          <Route path='*' component={FourOhFour} />
+        </Route>
+
+      </Router>
+)
+
 ReactDOM.render(
   <App />,
   document.getElementById('react-app')
