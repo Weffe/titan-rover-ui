dc3809456475c9224ad7e39a1ed4171bc89fe051
weffe <rogelio_negrete@live.com>
2017-02-17 22:06:34 -0800

    starting to incorporate Ant Design framework

--- src/App.js
+++ src/App.js
@@ -3,17 +3,37 @@ import Navbar from './Navbar';
 import MissionElapsedTime from './MissionElapsedTime';
 import './App.css';
 import 'd3';
+import { Layout, Menu, Icon, Row, Col} from  'antd';
+const { Sider, Content } = Layout;
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 class App extends Component {
     render() {
         return (
-            <div id="root">
-                <Navbar/>
-                <div id="main-content">
+            <Layout>
+                <Sider>
+                    <Menu
+                        mode="inline"
+                    >
+                        <Menu.Item key="overview">
+                            <Icon type="mail" /> Overview
+                        </Menu.Item>
+                        <Menu.Item key="Test">
+                            <Icon type="mail" /> Test
+                        </Menu.Item>
+                        <Menu.Item key="wow">
+                            <Icon type="mail" /> wow
+                        </Menu.Item>
+
+                    </Menu>
+                </Sider>
+                <Content>
+                    Main content
                     <MissionElapsedTime/>
                     {this.props.children}
-                </div>
-            </div>
+                </Content>
+            </Layout>
         );
     }
 }
